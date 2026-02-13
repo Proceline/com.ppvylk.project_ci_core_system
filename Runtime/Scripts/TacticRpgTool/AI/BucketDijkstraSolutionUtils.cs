a097705042640a1045a8f4f1673b8161d944e600
Proceline <wqjiujiu@gmail.com>
2026-02-12 23:46:40 -0500

    Make sure Layer record of Bucket doesn't have onUnit cell

--- Runtime/Scripts/TacticRpgTool/AI/BucketDijkstraSolutionUtils.cs
+++ Runtime/Scripts/TacticRpgTool/AI/BucketDijkstraSolutionUtils.cs
@@ -161,8 +161,11 @@ namespace ProjectCI.CoreSystem.Runtime.TacticRpgTool.AI
                         field.Parent[vertex] = cell;
                         bucketQueue.Enqueue(newG, vertex);
 
-                        // 分层：记录“总代价为 newG 的格”
-                        field.AddCellToLayerOnIndex(newG, vertex);
+                        if (!vertex.GetUnitOnCell())
+                        {
+                            // Add cell into layer list of newG
+                            field.AddCellToLayerOnIndex(newG, vertex);
+                        }
                     }
                 }
             }
